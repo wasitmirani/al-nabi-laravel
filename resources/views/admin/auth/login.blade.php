96a0291d790b129e057d41b83d7c0eb904a429ba
Muhammadsohail125 <muhammadsohail26566@gmail.com>
2021-08-30 23:43:06 +0500

    picture changed

--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -6,10 +6,10 @@
     <title>{{ trans('auth.admin_login_title') }}</title>
 
     <!-- General CSS File -->
-    <link rel="stylesheet" href="/assets/admin/vendor/bootstrap/bootstrap.min.css"/>
-    <link rel="stylesheet" href="/assets/admin/vendor/fontawesome/css/all.min.css"/>
-    <link rel="stylesheet" href="/assets/admin/css/style.css">
-    <link rel="stylesheet" href="/assets/admin/css/components.css">
+    <link rel="stylesheet" href="{{asset('/assets/admin/vendor/bootstrap/bootstrap.min.css')}}"/>
+    <link rel="stylesheet" href="{{asset('/assets/admin/vendor/fontawesome/css/all.min.css')}}"/>
+    <link rel="stylesheet" href="{{asset('/assets/admin/css/style.css')}}">
+    <link rel="stylesheet" href="{{asset('/assets/admin/css/components.css')}}">
 </head>
 <body>
 
@@ -24,10 +24,10 @@
         <div class="d-flex flex-wrap align-items-stretch">
             <div class="col-lg-4 col-md-6 col-12 order-lg-1 min-vh-100 order-2 bg-white">
                 <div class="p-4 m-3">
-                    <img src="{{ $siteGeneralSettings['logo'] ?? '' }}" alt="logo" width="40%" class="mb-5 mt-2">
+                    {{-- <img src="{{ $siteGeneralSettings['logo'] ?? '' }}" alt="logo" width="40%" class="mb-5 mt-2">\ --}}
 
-                    <h4 class="text-dark font-weight-normal">{{ trans('auth.welcome') }} <span class="font-weight-bold">{{ $siteGeneralSettings['site_name'] ?? '' }} {{ trans('auth.admin_panel') }}</span></h4>
-
+                    {{-- <h4 class="text-dark font-weight-normal">{{ trans('auth.welcome') }} <span class="font-weight-bold">{{ $siteGeneralSettings['site_name'] ?? '' }} {{ trans('auth.admin_panel') }}</span></h4> --}}
+                    <h4>Welcome to An-Nabi-Institute LMS Admin Panel</h4>
                     <p class="text-muted">{{ trans('auth.admin_tagline') }}</p>
 
                     <form method="POST" action="{{url('/admin/login')}}" class="needs-validation" novalidate="">
@@ -72,7 +72,7 @@
                 </div>
             </div>
 
-            <div class="col-lg-8 col-12 order-lg-2 order-1 min-vh-100 background-walk-y position-relative overlay-gradient-bottom" data-background="{{ $getPageBackgroundSettings['admin_login'] ?? '' }}">
+            <div class="col-lg-8 col-12 order-lg-2 order-1 min-vh-100 background-walk-y position-relative overlay-gradient-bottom" data-background="{{asset('/frontend/assets/images/banner.jpg')}}">
                 <div class="absolute-bottom-left index-2">
                     <div class="text-light p-5 pb-2">
                         <div class="mb-5 pb-3">
@@ -85,17 +85,17 @@
     </section>
 </div>
 <!-- General JS Scripts -->
-<script src="/assets/admin/vendor/jquery/jquery-3.3.1.min.js"></script>
-<script src="/assets/admin/vendor/poper/popper.min.js"></script>
-<script src="/assets/admin/vendor/bootstrap/bootstrap.min.js"></script>
-<script src="/assets/admin/vendor/nicescroll/jquery.nicescroll.min.js"></script>
-<script src="/assets/admin/vendor/moment/moment.min.js"></script>
-<script src="/assets/admin/js/stisla.js"></script>
+<script src="{{asset('/assets/admin/vendor/jquery/jquery-3.3.1.min.js')}}"></script>
+<script src="{{asset('/assets/admin/vendor/poper/popper.min.js')}}"></script>
+<script src="{{asset('/assets/admin/vendor/bootstrap/bootstrap.min.js')}}"></script>
+<script src="{{asset('/assets/admin/vendor/nicescroll/jquery.nicescroll.min.js')}}"></script>
+<script src="{{asset('/assets/admin/vendor/moment/moment.min.js')}}"></script>
+<script src="{{asset('/assets/admin/js/stisla.js')}}"></script>
 
 
 <!-- Template JS File -->
-<script src="/assets/admin/js/scripts.js"></script>
-<script src="/assets/admin/js/custom.js"></script>
+<script src="{{asset('/assets/admin/js/scripts.js')}}"></script>
+<script src="{{asset('/assets/admin/js/custom.js')}}"></script>
 
 </body>
 </html>
